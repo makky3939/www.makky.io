de228d0a727501eb5d56d74bf7987984d1fc0306
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2017-04-08 19:09:30 +0900

    added base contents

---
+++ src/sass/font.js
@@ -0,0 +1,6 @@
+// require('font-awesome/fonts/fontawesome-webfont.eot');
+require('font-awesome/fonts/fontawesome-webfont.svg');
+require('font-awesome/fonts/fontawesome-webfont.ttf');
+require('font-awesome/fonts/fontawesome-webfont.woff');
+require('font-awesome/fonts/fontawesome-webfont.woff2');
+// require('font-awesome/fonts/FontAwesome.otf');
