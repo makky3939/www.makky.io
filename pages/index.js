f61a8210cb885f3551eb3da4d7f00b7fbe289b8c
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2020-03-06 19:54:37 +0900

    Added some layouts

--- pages/index.js
+++ pages/index.js
@@ -1,203 +1,111 @@
-import Head from 'next/head'
+import { Container, Row, Col, Card, CardBody, CardTitle, CardText } from 'reactstrap';
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Index = () => (
+  <Container>
+    <Row className="justify-content-center">
+      <Col md="6">
+        <Card>
+          <CardBody>
+            <CardTitle>
+              <h4>
+                Masaki Kobayashi
+              </h4>
+            </CardTitle>
+            <hr />
+            <CardText>
+              <p>
+                Doctoral student at University of Tsukuba (Graduate School of Library, Information and Media Studies)
+              </p>
+              <dl>
+                <dt>Research Topics:</dt>
+                <dd>
+                  Human Computation, Crowdsourcing, Human-AI Collaboration
+                </dd>
+                <dt>Interests:</dt>
+                <dd>
+                  Front-end Web, Site Reliability Engineering, Photography
+                </dd>
+              </dl>
+            </CardText>
+          </CardBody>
+        </Card>
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      </Col>
+    </Row>
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+    <Row>
+      <Col>
+        <h4>
+          Self Introduction
+        </h4>
+        <p>
+          I am a 1st-year doctoral student at the Graduate School of Library, Information and Media Studies, University of Tsukuba.
+          My current research interest is Human-Machine collaboration in Crowdsourcing for efficient microtask processing.
+          Currently, I am studying to propose task assignment algorithms for Human and AI workers satisfying the data quality requirement from requesters.
+        </p>
+        <p>
+          I am also interested in developing web applications and, I am fluent in JavaScript, TypeScript, Ruby, and Python.
+          I have experienced as a part-time engineer and an intern in more than ten companies.
+          At present, I am working on the projects of https://github.com/chainer/chainerui and http://crowd4u.org.
+        </p>
+      </Col>
+    </Row>
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    <Row>
+      <Col>
+        <h4>
+          Publications
+        </h4>
+        <ul>
+          <li>aaa</li>
+        </ul>
+      </Col>
+    </Row>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+    <Row>
+      <Col>
+        <h4>
+          Work Experience
+        </h4>
+        <ul>
+          <li>aaa</li>
+        </ul>
+      </Col>
+    </Row>
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+    <Row>
+      <Col>
+        <h4>
+          Teaching Experience
+        </h4>
+        <ul>
+          <li>aaa</li>
+        </ul>
+      </Col>
+    </Row>
 
-        <a
-          href="https://zeit.co/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Row>
+      <Col>
+        <h4>
+          Research Grants
+        </h4>
+        <ul>
+          <li>aaa</li>
+        </ul>
+      </Col>
+    </Row>
 
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
+    <Row>
+      <Col>
+        <h4>
+          Awards
+        </h4>
+        <ul>
+          <li>aaa</li>
+        </ul>
+      </Col>
+    </Row>
+  </Container>
 )
 
-export default Home
+export default Index
