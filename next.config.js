d49511d398c9bc4fc5a67cd265cf9b6b795d0c2f
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2020-07-09 01:08:50 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,2 +1,20 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({})
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+    mozjpeg: {
+      quality: 50,
+    },
+    optipng: {
+      optimizationLevel: 3,
+    },
+  }],
+
+  // your other plugins here
+  [
+    withSass, {}
+  ]
+]);
